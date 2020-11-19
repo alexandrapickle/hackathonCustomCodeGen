d09bdb727ee375fb832a71948ef679146cfd9b5f
alexandrapickle <alexandra.pickle@synapsewireless.com>
2020-11-19 11:13:44 -0600

    add files folder

--- error-component/src/error-component/index.ts
+++ error-component/src/error-component/index.ts
@@ -1,11 +1,12 @@
 import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import { Schema } from "../schema";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function errorComponent(_options: any): Rule {
+export function errorComponent(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const { name } = _options;
-    tree.create("/output/hello.js", `console.log('Hello ${name}!');`);
+    const { errorMessage } = _options;
+    tree.create("/output/error-component.ts", `console.log(${errorMessage});`);
     return tree;
   };
 }
